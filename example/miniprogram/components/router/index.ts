0f4c08290754c8d3ce8d8918fdac87e94b27a8ce
JerryC <huangjerryc@gmail.com>
2021-03-21 21:54:47 +0800

    fix: remove useless console.log

--- example/miniprogram/components/router/index.ts
+++ example/miniprogram/components/router/index.ts
@@ -1,34 +1,32 @@
-
 Component({
-    properties: {
-        path: String,
-        type: {
-            type: String,
-            value: 'gotoPage'
-        },
-        route: String,
-        query: Object,
-        delta: Number,
-        setData: Object,
+  properties: {
+    path: String,
+    type: {
+      type: String,
+      value: 'gotoPage',
     },
+    route: String,
+    query: Object,
+    delta: Number,
+    setData: Object,
+  },
 
-    methods: {
-        gotoPage(event) {
-            const router = getApp().router;
-            const { path, route, type, query} = this.data;
-            const toPath = route || path;
+  methods: {
+    gotoPage() {
+      const router = getApp().router;
+      const { path, route, type, query } = this.data;
+      const toPath = route || path;
 
-            if (['gotoPage', 'navigateTo', 'switchTab', 'redirectTo'].includes(type)) {
-                (router as any)[type](toPath, query);
-            }
+      if (
+        ['gotoPage', 'navigateTo', 'switchTab', 'redirectTo'].includes(type)
+      ) {
+        (router as any)[type](toPath, query);
+      }
 
-            if (type === 'navigateBack') {
-                console.log({event});
-                const { delta, setData } = this.data;
-                router.navigateBack({ delta }, { setData })
-            }
-
-        }
-    }
-
-})
+      if (type === 'navigateBack') {
+        const { delta, setData } = this.data;
+        router.navigateBack({ delta }, { setData });
+      }
+    },
+  },
+});
